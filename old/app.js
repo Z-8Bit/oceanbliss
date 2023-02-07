bc8bcfc1b610f166a59e4c3ad9e3a315959a5ed4
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2023-02-07 14:51:24 +0530

    add old

---
+++ old/app.js
@@ -0,0 +1,147 @@
+/*****************/
+/* EDITABLE INFO */
+/*****************/
+
+/* -------------------------------------------------------- */
+
+const NAME = "Zishaan";
+
+const CARDS = [
+  {
+    name: "Youtube",
+    icon: "ri-youtube-fill",
+    link: "https://www.youtube.com",
+  },
+  {
+    name: "Discord",
+    icon: "ri-discord-fill",
+    link: "https://discord.com/channels/@me",
+  },
+  {
+    name: "Spotify",
+    icon: "ri-spotify-fill",
+    link: "https://open.spotify.com/",
+  },
+  {
+    name: "GitHub",
+    icon: "ri-github-fill",
+    link: "https://github.com/Z-8Bit",
+  },
+  {
+    name: "Whatsapp",
+    icon: "ri-whatsapp-fill",
+    link: "https://web.whatsapp.com/",
+  },
+  {
+    name: "Twitter",
+    icon: "ri-twitter-fill",
+    link: "https://twitter.com",
+  },
+  {
+    name: "Instagram",
+    icon: "ri-instagram-fill",
+    link: "https://www.instagram.com/",
+  },
+  {
+    name: "ProtonMail",
+    icon: "ri-mail-fill",
+    link: "https://mail.proton.me/u/0/inbox",
+  },
+];
+
+/* -------------------------------------------------------- */
+
+/******************/
+/* CLOCK FUNCTION */
+/******************/
+
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const updateDate = () => {
+  // Create a new Date object and get the complete Date/Time information
+  const completeDate = new Date();
+
+  // Time Variables
+  let currentHour = formatDigit(completeDate.getHours());
+  let currentMinute = formatDigit(completeDate.getMinutes());
+
+  // Date Variables
+  let currentDay = completeDate.getDay();
+  let currentNumber = completeDate.getDate();
+  let currentMonth = completeDate.getMonth();
+  let currentYear = completeDate.getFullYear();
+
+  // Update the Time
+  currentTime.innerHTML = `${currentHour}:${currentMinute}`;
+
+  // Update the Date
+  currentDate.innerHTML = `${DAYS[currentDay]} ${currentNumber}, ${MONTHS[currentMonth]} ${currentYear}`;
+
+  // Create a Loop
+  setTimeout(() => {
+    updateDate();
+  }, 1000);
+};
+
+const formatDigit = (digit) => {
+  return digit > 9 ? `${digit}` : `0${digit}`;
+};
+
+/******************/
+/* CARDS FUNCTION */
+/******************/
+
+const printCards = () => {
+  for (const card of CARDS) {
+    let currentCard = document.createElement("a");
+    let currentCardText = document.createElement("p");
+    currentCardText.appendChild(document.createTextNode(card.name));
+    let currentCardIcon = document.createElement("i");
+    currentCardIcon.classList.add(card.icon);
+
+    // Style the Card Element
+    currentCard.classList.add("card");
+    currentCard.href = card.link;
+
+    // Style the Icon
+    currentCardIcon.classList.add("card__icon");
+
+    // Style the Text
+    currentCardText.classList.add("card__name");
+
+    currentCard.append(currentCardIcon);
+    currentCard.append(currentCardText);
+    cardContainer.appendChild(currentCard);
+  }
+};
+
+/****************/
+/* STARTER CODE */
+/****************/
+
+userName.innerTEXT = NAME;
+printCards();
+updateDate();
