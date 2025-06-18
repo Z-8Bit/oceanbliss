28f68d4b42f0548e97defffa874f619c1c463cbe
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2025-06-18 13:37:58 +0530

    remove apple music

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const NAME = "Zishaan";
 
 const CARDS = [
   {
-    name: "Apple Music",
-    icon: "ri-music-2-fill",
-    link: "https://music.apple.com/in/library/all-playlists/",
+    name: "Youtube",
+    icon: "ri-youtube-fill",
+    link: "https://www.youtube.com/",
   },
   {
     name: "Spotify",
